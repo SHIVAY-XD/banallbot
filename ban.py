4e5900ec8203ff76e801c2dcf4f6bed3c810f382
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-11-05 23:54:45 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -1,197 +1,60 @@
-#  Copyright (c) 2022 @TG_STARWORLD - STARWORLD
-# Telegram Ban All Bot 
-# Creator - STARWORLD
+import os
+os.system("pip3 install pyrogram==1.4.16")
+os.system("pip3 install TgCrypto")
+os.system("pip3 install async-lru")
+os.system("pip3 install PySocks")
+os.system("pip3 install pyaes")
+
+import asyncio
+from pyrogram import Client,filters, idle
+from pyrogram.types import *
+from config import API_ID, API_HASH, BOT_TOKEN
 
 import logging
-import re
-import os
-import sys
-import asyncio
-from telethon import TelegramClient, events
-import telethon.utils
-from telethon.tl import functions
-from telethon.tl.functions.channels import LeaveChannelRequest
-from asyncio import sleep
-from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins, ChatAdminRights
-from telethon.tl.functions.channels import EditBannedRequest
-from datetime import datetime
-from var import Var
-from time import sleep
-from telethon.errors.rpcerrorlist import FloodWaitError
-from telethon.tl import functions
-from telethon.tl.types import (
-    ChannelParticipantsAdmins,
-    ChannelParticipantsKicked,
-    ChatBannedRights,
-    UserStatusEmpty,
-    UserStatusLastMonth,
-    UserStatusLastWeek,
-    UserStatusOffline,
-    UserStatusOnline,
-    UserStatusRecently,
+from pyrogram.errors import (
+    ChatAdminRequired
 )
 
-RIGHTS = ChatBannedRights(
-    until_date=None,
-    view_messages=True,
-    send_messages=True,
-    send_media=True,
-    send_stickers=True,
-    send_gifs=True,
-    send_games=True,
-    send_inline=True,
-    embed_links=True,
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+API_ID = API_ID
+API_HASH = API_HASH
+BOT_TOKEN = BOT_TOKEN
+
+blaze = Client(
+            ":memory:",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN
 )
 
-
-logging.basicConfig(level=logging.INFO)
-
-print("Starting.....")
-
-Riz = TelegramClient('Riz', Var.API_ID, Var.API_HASH).start(bot_token=Var.BOT_TOKEN)
-
-
-SUDO_USERS = []
-for x in Var.SUDO: 
-    SUDO_USERS.append(x)
-
-@Riz.on(events.NewMessage(pattern="^/ping"))  
-async def ping(e):
-    if e.sender_id in SUDO_USERS:
-        start = datetime.now()
-        text = "Pong!"
-        event = await e.reply(text, parse_mode=None, link_preview=None )
-        end = datetime.now()
-        ms = (end-start).microseconds / 1000
-        await event.edit(f"**I'm On** \n\n __Pong__ !! `{ms}` ms")
-
-
-@Riz.on(events.NewMessage(pattern="^/kickall"))
-async def kickall(event):
-   if event.sender_id in SUDO_USERS:
-     if not event.is_group:
-         Reply = f"Noob !! Use This Cmd in Group."
-         await event.reply(Reply)
-     else:
-         await event.delete()
-         RiZ = await event.get_chat()
-         RiZoeLop = await event.client.get_me()
-         admin = RiZ.admin_rights
-         creator = RiZ.creator
-         if not admin and not creator:
-              return await event.reply("I Don't have sufficient Rights !!")
-         RiZoeL = await Riz.send_message(event.chat_id, "**WAIT !! I'm BUSYY**")
-         admins = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsAdmins)
-         admins_id = [i.id for i in admins]
-         all = 0
-         kimk = 0
-         async for user in event.client.iter_participants(event.chat_id):
-             all += 1
-             try:
-                if user.id not in admins_id:
-                    await event.client.kick_participant(event.chat_id, user.id)
-                    kimk += 1
-                    await asyncio.sleep(0.1)
-             except Exception as e:
-                    print(str(e))
-                    await asyncio.sleep(0.1)
-         await RiZoeL.edit(f"**Users Kicked Successfully ! \n\n Kicked:** `{kimk}` \n **Total:** `{all}`")
-    
-
-@Riz.on(events.NewMessage(pattern="^/loveall"))
-async def banall(event):
-   if event.sender_id in SUDO_USERS:
-     if not event.is_group:
-         Reply = f"Noob !! Use This Cmd in Group."
-         await event.reply(Reply)
-     else:
-         await event.delete()
-         RiZ = await event.get_chat()
-         RiZoeLop = await event.client.get_me()
-         admin = RiZ.admin_rights
-         creator = RiZ.creator
-         if not admin and not creator:
-              return await event.reply("I Don't have sufficient Rights !!")
-         RiZoeL = await Riz.send_message(event.chat_id, "**I LOVE ALL!! **")
-         admins = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsAdmins)
-         admins_id = [i.id for i in admins]
-         all = 0
-         bann = 0
-         async for user in event.client.iter_participants(event.chat_id):
-             all += 1
-             try:
-               if user.id not in admins_id:
-                    await event.client(EditBannedRequest(event.chat_id, user.id, RIGHTS))
-                    bann += 1
-                    await asyncio.sleep(0.1)
-             except Exception as e:
-                   print(str(e))
-                   await asyncio.sleep(0.1)
-         await RiZoeL.edit(f"**STAR PAPA AAYA CHOD KA CHALA GAYE MADE BY @TG_STARWORLD! \n\n Banned Users:** `{bann}` \n **Total Users:** `{all}`")
-
-    
-@Riz.on(events.NewMessage(pattern="^/unbanall"))
-async def unban(event):
-   if event.sender_id in SUDO_USERS:
-     if not event.is_group:
-         Reply = f"Noob !! Use This Cmd in Group."
-         await event.reply(Reply)
-     else:
-         msg = await event.reply("Searching Participant Lists.")
-         p = 0
-         async for i in event.client.iter_participants(event.chat_id, filter=ChannelParticipantsKicked, aggressive=True):
-              rights = ChatBannedRights(until_date=0, view_messages=False)
-              try:
-                await event.client(functions.channels.EditBannedRequest(event.chat_id, i, rights))
-              except FloodWaitError as ex:
-                 print(f"sleeping for {ex.seconds} seconds")
-                 sleep(ex.seconds)
-              except Exception as ex:
-                 await msg.edit(str(ex))
-              else:
-                  p += 1
-         await msg.edit("{}: {} unbanned".format(event.chat_id, p))
-
-
-@Riz.on(events.NewMessage(pattern="^/leave"))
-async def _(e):
-    if e.sender_id in SUDO_USERS:
-        rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
-        if len(e.text) > 7:
-            bc = rizoel[0]
-            bc = int(bc)
-            text = "Leaving....."
-            event = await e.reply(text, parse_mode=None, link_preview=None )
-            try:
-                await event.client(LeaveChannelRequest(bc))
-                await event.edit("Succesfully Left")
-            except Exception as e:
-                await event.edit(str(e))   
-        else:
-            bc = e.chat_id
-            text = "Leaving....."
-            event = await e.reply(text, parse_mode=None, link_preview=None )
-            try:
-                await event.client(LeaveChannelRequest(bc))
-                await event.edit("Succesfully Left")
-            except Exception as e:
-                await event.edit(str(e))   
-          
-
-@Riz.on(events.NewMessage(pattern="^/restart"))
-async def restart(e):
-    if e.sender_id in SUDO_USERS:
-        text = "__Restarting__ !!!"
-        await e.reply(text, parse_mode=None, link_preview=None )
+@star.on_message(filters.command("banall") & filters.group)
+def banall(bot,message):
+    logging.info("new chat {}".format(message.chat.id))
+    logging.info("getting memebers from {}".format(message.chat.id))
+    a= bot.iter_chat_members(message.chat.id)
+    for i in a:
         try:
-            await Riz.disconnect()
+            bot.ban_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
         except Exception:
-            pass
-        os.execl(sys.executable, sys.executable, *sys.argv)
-        quit()
+            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+            
+    logging.info("process completed")
+
+@star.on_message(filters.command("alive"))
+async def alive(bot, message):
+    await message.reply("**Am Alive ❣️**\n\n𝙰 𝙶𝚁𝙾𝚄𝙿 𝙳𝙸𝚂𝚃𝚁𝚄𝙲𝚃𝙸𝙾𝙽 𝙱𝙾𝚃 𝙼𝙰𝙳𝙴 𝚆𝙸𝚃𝙷 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼 𝙵𝙾𝚁 𝙳𝙸𝚂𝚃𝚁𝙾𝚈𝙸𝙽𝙶 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 𝙲𝙷𝙰𝚃𝚂")
 
 
-print("\n\n")
-print("Your Ban All Bot Deployed Successfully ✅")
 
-Riz.run_until_disconnected()
+star.start()
+print("Client Started Successfully")
+idle()
+star.stop()
+print("GoodBye Stopping Banall.")
