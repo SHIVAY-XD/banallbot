a3eaf8c595ccf8128f8973b041ad8261ed7ba83a
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-11-06 10:02:01 +0530

    Delete var.py

--- var.py
+++
@@ -1,9 +0,0 @@
-from decouple import config
-from heroku3 import from_key
-from os import getenv
-
-class Var:
-    API_ID = config("API_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", None)
-    BOT_TOKEN = config("BOT_TOKEN", None)
-    SUDO = list(map(int, getenv("SUDO").split()))
